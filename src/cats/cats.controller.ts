d327575959eac0f2b55e343c3c233f703377ccd5
luan <los8@aluno.ifal.edu.br>
2023-08-29 16:20:54 -0300

    Adiciona as rotas  POST, DELETE e PUT

--- src/cats/cats.controller.ts
+++ src/cats/cats.controller.ts
@@ -1,6 +1,7 @@
-import { Controller, Get } from '@nestjs/common';
+import { Body, Controller, Delete, Get, HttpStatus, Param, ParseIntPipe, Post, Put, Res } from '@nestjs/common';
 import { CatsService } from './cats.service';
 import { Cat } from './interfaces/cat.interface';
+import { Response } from 'express';
 
 @Controller('cats')
 export class CatsController {
@@ -8,9 +9,48 @@ export class CatsController {
     constructor(private catsService: CatsService) {}
 
     @Get()
-    findAll(): Cat[]{
+    findAll(): Cat[] {
         return this.catsService.findAll();
     }
 
+        @Get(':id')
+    findOne(@Param('id', ParseIntPipe) id: number, @Res() res: Response) {
+        const gatoEncontrado = this.catsService.findById(id);
+        if(gatoEncontrado) {
+            res.status(HttpStatus.OK).json(gatoEncontrado);
+        }else{
+            res.status(HttpStatus.NOT_FOUND).send;
+        }
+
+    }
+
+    @Delete(':id')
+    remove(@Param('id', ParseIntPipe) id: number, @Res() res: Response ) {
+        const indexGatoEncontrado = this.catsService.findIndexById(id);
+            if(indexGatoEncontrado >= 0) {
+                this.catsService.deleteByIndex(indexGatoEncontrado);
+                res.status(HttpStatus.NO_CONTENT).send();
+            } else{
+                res.status(HttpStatus.NOT_FOUND).send();
+            }
+    }
+
+    @Post()
+     create( @Body() cat: Cat, @Res() res: Response) {
+        this.catsService.create(cat);
+        res.status(HttpStatus.CREATED).json(cat);
+     }
+
+     @Put(':id')
+    update( @Param('id', ParseIntPipe) id: number, @Body() cat: Cat, @Res() res: Response) {
+        const indexGatoEncontrado = this.catsService.findIndexById(id);
+        if(indexGatoEncontrado >= 0){
+            this.catsService.update(indexGatoEncontrado, cat);
+            res.status(HttpStatus.NO_CONTENT).send();
+        } else {
+            res.status(HttpStatus.NOT_FOUND).send();
+        }
+        return `Atualizando gato com id ${id} e valores ${JSON.stringify(cat)}`
+    }
 
 }
